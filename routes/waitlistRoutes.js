3d3a5c72ce8e90eb324c7c3064242ce52a2f6ade
nikhil.gohil1 <nikhil.gohil@deptagency.com>
2025-04-05 20:37:27 +0530

    debugging

--- routes/waitlistRoutes.js
+++ routes/waitlistRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const { joinWaitlist } = require('../controllers/waitlistController')
 
 router.post('/', joinWaitlist)
+router.get('/new',  (req, res) =>{
+    res.send('This is the new route!');
+})
+
 
 module.exports = router
