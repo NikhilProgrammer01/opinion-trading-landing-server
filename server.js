e6bfd889d0a45d90d7bac00ebdf9e959e53628c0
nikhil.gohil1 <nikhil.gohil@deptagency.com>
2025-04-05 20:26:20 +0530

    added verce json file

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const waitlistRoutes = require('./routes/waitlistRoutes')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-// Middleware
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173/', // replace with your actual frontend domain
+  credentials: true
+}));
+
 app.use(express.json())
 
 // Routes
