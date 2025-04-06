016819b2b6c2ced8a940e408fc066eb88dcc1117
nikhil.gohil1 <nikhil.gohil@deptagency.com>
2025-04-06 13:13:26 +0530

    seperated the db code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 
 // CORS setup
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://opinion-trading-landing-client-side.vercel.app'],
+  origin: ['http://localhost:5173', 'https://opinion-trading-landing-client-side-9ilu.vercel.app'],
   credentials: true
 }));
 
